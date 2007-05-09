2f5a71aecfad85ae5e6faab52fac302dca6bd866
mlampe <mlampe@b63d296a-7662-4df4-3098-00f9c2c9103a>
2007-05-09 17:09:58 +0000

    release branch for fc-0.2

---
+++ fc/branches/fc-0.2/fc-commons/src/main/java/org/fosstrak/ale/xsd/ale/epcglobal/ECReportGroupList.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
+/**
+ * ECReportGroupList.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis 1.3 Oct 05, 2005 (05:23:37 EDT) WSDL2Java emitter.
+ */
+
+package org.accada.ale.xsd.ale.epcglobal;
+
+public class ECReportGroupList  implements java.io.Serializable, org.apache.axis.encoding.AnyContentType {
+    private org.accada.ale.xsd.ale.epcglobal.ECReportGroupListMember[] member;
+
+    private org.accada.ale.xsd.ale.epcglobal.ECReportGroupListExtension extension;
+
+    private org.apache.axis.message.MessageElement [] _any;
+
+    public ECReportGroupList() {
+    }
+
+    public ECReportGroupList(
+           org.accada.ale.xsd.ale.epcglobal.ECReportGroupListMember[] member,
+           org.accada.ale.xsd.ale.epcglobal.ECReportGroupListExtension extension,
+           org.apache.axis.message.MessageElement [] _any) {
+           this.member = member;
+           this.extension = extension;
+           this._any = _any;
+    }
+
+
+    /**
+     * Gets the member value for this ECReportGroupList.
+     * 
+     * @return member
+     */
+    public org.accada.ale.xsd.ale.epcglobal.ECReportGroupListMember[] getMember() {
+        return member;
+    }
+
+
+    /**
+     * Sets the member value for this ECReportGroupList.
+     * 
+     * @param member
+     */
+    public void setMember(org.accada.ale.xsd.ale.epcglobal.ECReportGroupListMember[] member) {
+        this.member = member;
+    }
+
+    public org.accada.ale.xsd.ale.epcglobal.ECReportGroupListMember getMember(int i) {
+        return this.member[i];
+    }
+
+    public void setMember(int i, org.accada.ale.xsd.ale.epcglobal.ECReportGroupListMember _value) {
+        this.member[i] = _value;
+    }
+
+
+    /**
+     * Gets the extension value for this ECReportGroupList.
+     * 
+     * @return extension
+     */
+    public org.accada.ale.xsd.ale.epcglobal.ECReportGroupListExtension getExtension() {
+        return extension;
+    }
+
+
+    /**
+     * Sets the extension value for this ECReportGroupList.
+     * 
+     * @param extension
+     */
+    public void setExtension(org.accada.ale.xsd.ale.epcglobal.ECReportGroupListExtension extension) {
+        this.extension = extension;
+    }
+
+
+    /**
+     * Gets the _any value for this ECReportGroupList.
+     * 
+     * @return _any
+     */
+    public org.apache.axis.message.MessageElement [] get_any() {
+        return _any;
+    }
+
+
+    /**
+     * Sets the _any value for this ECReportGroupList.
+     * 
+     * @param _any
+     */
+    public void set_any(org.apache.axis.message.MessageElement [] _any) {
+        this._any = _any;
+    }
+
+    private java.lang.Object __equalsCalc = null;
+    public synchronized boolean equals(java.lang.Object obj) {
+        if (!(obj instanceof ECReportGroupList)) return false;
+        ECReportGroupList other = (ECReportGroupList) obj;
+        if (obj == null) return false;
+        if (this == obj) return true;
+        if (__equalsCalc != null) {
+            return (__equalsCalc == obj);
+        }
+        __equalsCalc = obj;
+        boolean _equals;
+        _equals = true && 
+            ((this.member==null && other.getMember()==null) || 
+             (this.member!=null &&
+              java.util.Arrays.equals(this.member, other.getMember()))) &&
+            ((this.extension==null && other.getExtension()==null) || 
+             (this.extension!=null &&
+              this.extension.equals(other.getExtension()))) &&
+            ((this._any==null && other.get_any()==null) || 
+             (this._any!=null &&
+              java.util.Arrays.equals(this._any, other.get_any())));
+        __equalsCalc = null;
+        return _equals;
+    }
+
+    private boolean __hashCodeCalc = false;
+    public synchronized int hashCode() {
+        if (__hashCodeCalc) {
+            return 0;
+        }
+        __hashCodeCalc = true;
+        int _hashCode = 1;
+        if (getMember() != null) {
+            for (int i=0;
+                 i<java.lang.reflect.Array.getLength(getMember());
+                 i++) {
+                java.lang.Object obj = java.lang.reflect.Array.get(getMember(), i);
+                if (obj != null &&
+                    !obj.getClass().isArray()) {
+                    _hashCode += obj.hashCode();
+                }
+            }
+        }
+        if (getExtension() != null) {
+            _hashCode += getExtension().hashCode();
+        }
+        if (get_any() != null) {
+            for (int i=0;
+                 i<java.lang.reflect.Array.getLength(get_any());
+                 i++) {
+                java.lang.Object obj = java.lang.reflect.Array.get(get_any(), i);
+                if (obj != null &&
+                    !obj.getClass().isArray()) {
+                    _hashCode += obj.hashCode();
+                }
+            }
+        }
+        __hashCodeCalc = false;
+        return _hashCode;
+    }
+
+    // Type metadata
+    private static org.apache.axis.description.TypeDesc typeDesc =
+        new org.apache.axis.description.TypeDesc(ECReportGroupList.class, true);
+
+    static {
+        typeDesc.setXmlType(new javax.xml.namespace.QName("urn:epcglobal:ale:xsd:1", "ECReportGroupList"));
+        org.apache.axis.description.ElementDesc elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("member");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "member"));
+        elemField.setXmlType(new javax.xml.namespace.QName("urn:epcglobal:ale:xsd:1", "ECReportGroupListMember"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        elemField.setMaxOccursUnbounded(true);
+        typeDesc.addFieldDesc(elemField);
+        elemField = new org.apache.axis.description.ElementDesc();
+        elemField.setFieldName("extension");
+        elemField.setXmlName(new javax.xml.namespace.QName("", "extension"));
+        elemField.setXmlType(new javax.xml.namespace.QName("urn:epcglobal:ale:xsd:1", "ECReportGroupListExtension"));
+        elemField.setMinOccurs(0);
+        elemField.setNillable(false);
+        typeDesc.addFieldDesc(elemField);
+    }
+
+    /**
+     * Return type metadata object
+     */
+    public static org.apache.axis.description.TypeDesc getTypeDesc() {
+        return typeDesc;
+    }
+
+    /**
+     * Get Custom Serializer
+     */
+    public static org.apache.axis.encoding.Serializer getSerializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanSerializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+    /**
+     * Get Custom Deserializer
+     */
+    public static org.apache.axis.encoding.Deserializer getDeserializer(
+           java.lang.String mechType, 
+           java.lang.Class _javaType,  
+           javax.xml.namespace.QName _xmlType) {
+        return 
+          new  org.apache.axis.encoding.ser.BeanDeserializer(
+            _javaType, _xmlType, typeDesc);
+    }
+
+}
