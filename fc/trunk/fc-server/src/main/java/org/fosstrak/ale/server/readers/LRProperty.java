533e2b39add0a06be4dda7fb2ae33d41e405872c
sawielan <sawielan@b63d296a-7662-4df4-3098-00f9c2c9103a>
2007-09-13 12:09:58 +0000

    New Logical Reader API
    LRProperty according the EPC Standard

---
+++ fc/trunk/fc-server/src/main/java/org/fosstrak/ale/server/readers/LRProperty.java
@@ -0,0 +1,67 @@
+package org.accada.ale.server.readers;
+
+
+
+
+
+public class LRProperty {
+
+
+    protected String name;
+    
+    protected String value;
+
+    public LRProperty(String name, String value){
+    	this.name = name;
+    	this.value = value;
+    }
+    
+    /**
+     * Gets the value of the name property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Sets the value of the name property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setName(String value) {
+        this.name = value;
+    }
+
+    /**
+     * Gets the value of the value property.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getValue() {
+        return value;
+    }
+
+    /**
+     * Sets the value of the value property.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+}
