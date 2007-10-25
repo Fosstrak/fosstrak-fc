50ac7d06f3c5644ae849ccbd0cdd14e3b5380892
haennimi <haennimi@b63d296a-7662-4df4-3098-00f9c2c9103a>
2007-10-25 08:40:36 +0000

    LogicalReaderManager: - Changed exception declaration to fit ALE 1.1 spec. LRProperty: - Checkstyle adjustments, license text added.

--- fc/trunk/fc-server/src/main/java/org/fosstrak/ale/server/readers/LRProperty.java
+++ fc/trunk/fc-server/src/main/java/org/fosstrak/ale/server/readers/LRProperty.java
@@ -1,16 +1,42 @@
+/*
+ * Copyright (C) 2007 ETH Zurich
+ *
+ * This file is part of Accada (www.accada.org).
+ *
+ * Accada is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License version 2.1, as published by the Free Software Foundation.
+ *
+ * Accada is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with Accada; if not, write to the Free
+ * Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA  02110-1301  USA
+ */
+
 package org.accada.ale.server.readers;
 
 
 
 
-
+/** The LRProperty is a name-value pair describing a attribute of a logical reader.
+ * 
+ * @author sawielan
+ * @author haennimi
+ */
 public class LRProperty {
 
-
+	/** The name of the LRProperty.*/
     protected String name;
     
+    /** The value of the LRProperty.*/
     protected String value;
 
+    /**Constructor of the LRProperty.*/
     public LRProperty(String name, String value){
     	this.name = name;
     	this.value = value;
