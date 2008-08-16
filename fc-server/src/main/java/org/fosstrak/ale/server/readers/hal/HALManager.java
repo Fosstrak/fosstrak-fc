0c297ce770c09fef3bca8f8eae8e6211424bd6cd
sawielan <samuel.wieland@gmail.com>
2008-08-16 19:24:02 +0000

    fixed sourceforge bug 2054573.
    HALAdaptor did not correctly invoke the underlying HAL implementation (basically only simulator hal was able to be instanciated).
    - fixed the issue
    - updated documentation with more details

--- fc-server/src/main/java/org/fosstrak/ale/server/readers/hal/HALManager.java
+++ fc-server/src/main/java/org/fosstrak/ale/server/readers/hal/HALManager.java
@@ -1,5 +1,7 @@
 package org.fosstrak.ale.server.readers.hal;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -80,9 +82,10 @@ public class HALManager {
 	 * a reference to the reader is returned. otherwise a new reader is created.
 	 * @param halName the name of the HAL reader that shall be created.
 	 * @param propFile the properties file for the reader.
+	 * @param the class to invoke that implements the hal (eg. org.fosstrak.hal.impl.sim.SimulatorController).
 	 * @return an instance of a HardwareAbstraction reader.
 	 */
-	public synchronized HardwareAbstraction define(String halName, String propFile) {
+	public synchronized HardwareAbstraction define(String halName, String propFile, String ImplementingClass) {
 		HardwareAbstraction hal = null;
 		
 		if (hals.containsKey(halName)) {
@@ -92,11 +95,50 @@ public class HALManager {
 			
 			log.debug("reusing running HAL instance: " + halName);
 		} else {
-			// create the HAL device
-			hal = new SimulatorController(halName, propFile);
-			hals.put(halName, new HALManagerEntry(hal));
 			
-			log.debug("creating new HAL instance: " + halName);
+			try {
+				Class cls = Class.forName(ImplementingClass);
+				// get the constructor
+				Constructor ctor = cls.getConstructor(String.class, String.class);
+				
+				// invoke the constructor
+				Object instnc = ctor.newInstance(halName, propFile);
+
+				if (instnc instanceof HardwareAbstraction) {
+					hal = (HardwareAbstraction) instnc;
+					
+					hals.put(halName, new HALManagerEntry(hal));
+					
+					log.debug("creating new HAL instance: " + halName);
+					return hal;
+				}
+				
+				return null;
+		
+			} catch (ClassNotFoundException e) {
+				log.error(String.format("implementing class '%s' not found!", ImplementingClass));
+				
+			}	catch (SecurityException e) {
+				log.error(String.format("security exception when creating instance of '%s'!", ImplementingClass));
+				
+			} catch (NoSuchMethodException e) {
+				log.error(String.format("class '%s' has no constructor of form (String halName, String configFile)!", ImplementingClass));
+			
+			} catch (IllegalArgumentException e) {
+				log.error(String.format("illegal arguments when invoking constructor on class '%s'!", ImplementingClass));
+				
+			} catch (InstantiationException e) {
+				log.error(String.format("could not create instance of class '%s'!", ImplementingClass));
+				
+			} catch (IllegalAccessException e) {
+				log.error(String.format("illegal class exception when creating instance of '%s'!", ImplementingClass));
+				
+			} catch (InvocationTargetException e) {
+				log.error(String.format("could not invoke constructor on class '%s'!", ImplementingClass));
+				
+			}
+
+			return null;
 		}
 		
 		return hal;
